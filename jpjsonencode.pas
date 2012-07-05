803ea34369db4d6f5f348866c17ae5f361a92c62
Jean Patrick <jpsoft-sac-pa@hotmail.com>
2012-07-05 00:14:15 -0300

    Version 1.0
    Signed-off-by: Jean Patrick <jpsoft-sac-pa@hotmail.com>

--- jpjsonencode.pas
+++ jpjsonencode.pas
@@ -1,3 +1,15 @@
+{
+JPJSON Encode
+
+Version: 1.0
+
+Author: JEAN PATRICK - www.jpsoft.com.br - jpsoft-sac-pa@hotmail.com
+
+Date: 2012-07-05
+
+Licence: Free and OpenSource
+}
+
 unit jpjsonencode;
 
 {$mode objfpc}{$H+}
@@ -5,32 +17,34 @@ unit jpjsonencode;
 interface
 
 uses
-  Classes, SysUtils, db;
+  SysUtils, DB;
 
-Type
+type
 
   { TJPJSONEncode }
 
-  TJPJSONEncode = Class
-    public
-      function DataSetToJSONString(DataSet: TDataSet): String;
-      function DataSetToJSONString(DataSet: TDataSet; aFields: Array of String): String;
-    private
-      function EscapeJSONString(sJSON: String): String;
-    end;
+  TJPJSONEncode = class
+  public
+    function DataSetToJSONString(DataSet: TDataSet): string;
+    function DataSetToJSONString(DataSet: TDataSet; aFields: array of string): string;
+  private
+    function EscapeJSONString(sJSON: string): string;
+  end;
 
 
 implementation
 
 { TJPJSONEncode }
 
-function TJPJSONEncode.DataSetToJSONString(DataSet: TDataSet): String;
+function TJPJSONEncode.DataSetToJSONString(DataSet: TDataSet): string;
 var
-  i: Integer;
+  i: integer;
 begin
   Result := '';
-  if (DataSet.IsEmpty) then Exit;
-  if not(DataSet.Active) then begin
+  if (DataSet.IsEmpty) then
+    Exit;
+  if not (DataSet.Active) then
+  begin
     try
       DataSet.Open;
     except
@@ -38,39 +52,50 @@ begin
     end;
   end;
   DataSet.First;
-  while not(DataSet.EOF) do
+  while not (DataSet.EOF) do
   begin
     Result += '{';
     for i := 0 to DataSet.FieldCount - 1 do
     begin
-      if (i > 0) then Result +=',';
-      Result += '"'+DataSet.Fields[i].FieldName+'":';
-      if DataSet.Fields[i].IsNull then Result += 'null' else
-        begin
-          case DataSet.Fields[i].DataType of
-            ftSmallint,ftInteger: Result += DataSet.Fields[i].AsString;
-            ftFloat: Result += FloatToStr(DataSet.Fields[i].AsFloat);
-            ftBoolean: if (DataSet.Fields[i].AsBoolean) then Result += 'true' else Result += 'false';
+      if (i > 0) then
+        Result += ',';
+      Result += '"' + DataSet.Fields[i].FieldName + '":';
+      if DataSet.Fields[i].IsNull then
+        Result += 'null'
+      else
+      begin
+        case DataSet.Fields[i].DataType of
+          ftSmallint, ftInteger: Result += DataSet.Fields[i].AsString;
+          ftFloat: Result += FloatToStr(DataSet.Fields[i].AsFloat);
+          ftBoolean: if (DataSet.Fields[i].AsBoolean) then
+              Result += 'true'
             else
-              Result += '"'+EscapeJSONString(DataSet.Fields[i].AsString)+'"';
-          end;
+              Result += 'false';
+          else
+            Result += '"' + EscapeJSONString(DataSet.Fields[i].AsString) + '"';
         end;
+      end;
     end;
     Result += '}';
     DataSet.Next;
-    if not(DataSet.EOF) then Result += ',';
+    if not (DataSet.EOF) then
+      Result += ',';
   end;
-  Result := '['+Result+']';
+  Result := '[' + Result + ']';
 end;
 
 function TJPJSONEncode.DataSetToJSONString(DataSet: TDataSet;
-  aFields: Array of String): String;
+  aFields: array of string): string;
 var
-  i: Integer;
+  i: integer;
 begin
   Result := '';
-  if (DataSet.IsEmpty) then Exit;
-  if not(DataSet.Active) then begin
+  if (DataSet.IsEmpty) then
+    Exit;
+  if (Length(aFields) = 0) then
+    Exit;
+  if not (DataSet.Active) then
+  begin
     try
       DataSet.Open;
     except
@@ -78,42 +103,53 @@ begin
     end;
   end;
   DataSet.First;
-  while not(DataSet.EOF) do
+  while not (DataSet.EOF) do
   begin
     Result += '{';
-    for i := 0 to Length(aFields) - 1 do
-    begin
-      if (i > 0) then Result +=',';
-      Result += '"'+DataSet.FieldByName(aFields[i]).FieldName+'":';
-      if DataSet.FieldByName(aFields[i]).IsNull then Result += 'null' else
+    try
+      for i := 0 to Length(aFields) - 1 do
+      begin
+        if (i > 0) then
+          Result += ',';
+        Result += '"' + DataSet.FieldByName(aFields[i]).FieldName + '":';
+        if DataSet.FieldByName(aFields[i]).IsNull then
+          Result += 'null'
+        else
         begin
           case DataSet.FieldByName(aFields[i]).DataType of
-            ftSmallint,ftInteger: Result += DataSet.FieldByName(aFields[i]).AsString;
+            ftSmallint, ftInteger: Result += DataSet.FieldByName(aFields[i]).AsString;
             ftFloat: Result += FloatToStr(DataSet.FieldByName(aFields[i]).AsFloat);
-            ftBoolean: if (DataSet.FieldByName(aFields[i]).AsBoolean) then Result += 'true' else Result += 'false';
+            ftBoolean: if (DataSet.FieldByName(aFields[i]).AsBoolean) then
+                Result += 'true'
+              else
+                Result += 'false';
             else
-              Result += '"'+EscapeJSONString(DataSet.FieldByName(aFields[i]).AsString)+'"';
+              Result += '"' + EscapeJSONString(DataSet.FieldByName(
+                aFields[i]).AsString) + '"';
           end;
         end;
+      end;
+    except
+      raise Exception.Create('JPJSONEncode error: Unable to Fields!');
     end;
     Result += '}';
     DataSet.Next;
-    if not(DataSet.EOF) then Result += ',';
+    if not (DataSet.EOF) then
+      Result += ',';
   end;
-  Result := '['+Result+']';
+  Result := '[' + Result + ']';
 end;
 
-function TJPJSONEncode.EscapeJSONString(sJSON: String): String;
+function TJPJSONEncode.EscapeJSONString(sJSON: string): string;
 begin
-  Result := StringReplace(sJSON,'\','\\',[rfReplaceAll]);
-  Result := StringReplace(Result,'/','\/',[rfReplaceAll]);
-  Result := StringReplace(Result,'"','\"',[rfReplaceAll]);
-  Result := StringReplace(Result,#8 ,'\b',[rfReplaceAll]);
-  Result := StringReplace(Result,#9 ,'\t',[rfReplaceAll]);
-  Result := StringReplace(Result,#10,'\n',[rfReplaceAll]);
-  Result := StringReplace(Result,#12,'\f',[rfReplaceAll]);
-  Result := StringReplace(Result,#13,'\r',[rfReplaceAll]);
+  Result := StringReplace(sJSON, '\', '\\', [rfReplaceAll]);
+  Result := StringReplace(Result, '/', '\/', [rfReplaceAll]);
+  Result := StringReplace(Result, '"', '\"', [rfReplaceAll]);
+  Result := StringReplace(Result, #8, '\b', [rfReplaceAll]);
+  Result := StringReplace(Result, #9, '\t', [rfReplaceAll]);
+  Result := StringReplace(Result, #10, '\n', [rfReplaceAll]);
+  Result := StringReplace(Result, #12, '\f', [rfReplaceAll]);
+  Result := StringReplace(Result, #13, '\r', [rfReplaceAll]);
 end;
 
 end.
-
